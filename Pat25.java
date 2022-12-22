76c3fd7be9b946a5ce72114df4be3e351e65046f
Vishal-6789 <121079012+Vishal-6789@users.noreply.github.com>
2022-12-22 11:35:05 +0530

    Add files via upload

---
+++ Pat25.java
@@ -0,0 +1,33 @@
+package Patterns;
+
+public class Pat_25{
+	public static void main(String[] args) {
+		int n = 5;
+		int row = 1;
+		int total_st =1;
+		int total_sp =n-1;
+		int p=1;
+		
+		
+		while (row <= n) {
+			int cnt_sp = 0;
+			while(cnt_sp< total_sp) {
+				System.out.print("  ");
+				cnt_sp++;
+			}
+			int cnt_st = 0;
+			while(cnt_st< total_st) {
+				System.out.print(p +"  " );
+				cnt_st++;
+				p++;
+			}			
+			row++;
+			
+			
+			total_sp--;
+			total_st++;
+			total_st++;
+			System.out.println();
+		}
+	}
+}
---
+++ pattern22.java
@@ -0,0 +1,48 @@
+package Patterns;
+
+public class Pat_22{
+	public static void main(String[] args) {
+		int n = 5;
+		int row = 1;
+		int total_st1=5;
+		int total_sp = -1;
+		int total_st2 = 4;
+		while (row <= n) {
+		  int cnt_st1=0;
+		  while(cnt_st1<total_st1){
+		    System.out.print("* ");
+		    cnt_st1++;
+		  }
+			int cnt_sp = 0;
+			while (cnt_sp < total_sp) {
+				System.out.print("  ");
+				cnt_sp++;
+			}
+//			*
+			int cnt_st2 = 0;
+			while (cnt_st2 < total_st2) {
+				System.out.print("* ");
+				cnt_st2++;
+			}
+			
+			if(row==1){
+			  total_st1--;
+			  total_sp++;
+			  total_sp++;
+			  total_st2=total_st2;
+			}
+			
+      
+			else{
+			  total_st1--;
+			total_sp++;
+			total_sp++;
+			
+			total_st2--;
+			}
+			row++;
+			System.out.println();
+		}
+}
+	
+}
