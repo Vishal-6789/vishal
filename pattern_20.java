c3fda33d374b9d1a2056e863360261cd3770bbba
Vishal-6789 <121079012+Vishal-6789@users.noreply.github.com>
2022-12-22 11:23:47 +0530

    Add files via upload

---
+++ pattern_20.java
@@ -0,0 +1,39 @@
+package Patterns;
+
+public class Pat_20{
+	public static void main(String[] args) {
+		int n = 7;
+		int row = 1;
+		int total_sp = 3;
+				int total_st=1;
+		while (row <= n) {
+			int cnt_sp = 0;
+			while (cnt_sp < total_sp) {
+				System.out.print("  ");
+				cnt_sp++;
+			}
+//			*
+			int cnt_st = 0;
+			while (cnt_st < total_st) {
+			  if(cnt_st==0||cnt_st==total_st-1)
+			{	System.out.print("* ");}
+			else{ System.out.print("  ");}
+				cnt_st++;
+			}
+      if(row< (n+1)/2){
+			total_sp--;
+        total_st++;
+         total_st++;
+      }
+			else{
+			total_sp++;
+			total_st--;
+			total_st--;;
+			}
+			
+			System.out.println();
+			row++;
+		}
+
+	}
+}
